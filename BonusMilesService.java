06be15b2a9f53396a6c45c8df52e8ab2a83877f7
Alexander Khramov <sssahahram51@gmail.com>
2021-05-19 18:56:44 +0300

    add files

---
+++ BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+
+        int Price = 10000;  // в рублях
+        int amount = 20; // в рублях
+
+        int miles = Price / amount;
+
+        return miles;
+
+    }
+}
