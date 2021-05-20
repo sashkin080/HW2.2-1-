ecf410cf3d222f6ced0276b8d72b826dee3accbb
Alexander Khramov <sssahahram51@gmail.com>
2021-05-20 17:40:04 +0300

    up

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -2,12 +2,8 @@ public class BonusMilesService {
     public int calculate(int cost) {
 
         int bonusMiles = 20;
-        int miles = cost/bonusMiles;
+        int miles = cost / bonusMiles;
 
         return miles;
-
     }
-
-
-
 }
