e0651523538f72e97ab55b8ddb1989e387455483
Alexander Khramov <sssahahram51@gmail.com>
2021-05-21 17:03:40 +0300

    failed

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int cost = 10000;
-        int miles = service.calculate (cost);
+        int miles = service.calculate(cost);
         System.out.println(miles);
     }
 }
